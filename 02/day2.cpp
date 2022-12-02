97055bf4c936b1050f2fcbba91d8ff29e2f80939
Fibbo <1519902+fibbo@users.noreply.github.com>
2022-12-02 08:28:56 +0100

    Day 2 and restructuring

---
+++ 02/day2.cpp
@@ -0,0 +1,66 @@
+#include "open_file.hpp"
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <iostream>
+#include <unordered_map>
+
+using namespace pgl::aoc;
+
+static constexpr int rockValue = 1;
+static constexpr int paperValue = 2;
+static constexpr int scissorsValue = 3;
+
+static constexpr int loss = 0;
+static constexpr int draw = 3;
+static constexpr int win = 6;
+
+// A Rock
+// B Paper
+// C Scissors
+static const std::unordered_map<std::string, uint32_t> outcomeScopeMapPart1 = {
+    {"A X", 4}, // Draw with Rock
+    {"A Y", 8}, // Win with Paper
+    {"A Z", 3}, // Loss with Scissors
+    {"B X", 1}, // Loss with Rock
+    {"B Y", 5}, // Draw with Paper
+    {"B Z", 9}, // Win with Scissors
+    {"C X", 7}, // Win with Rock
+    {"C Y", 2}, // Loss with Paper
+    {"C Z", 6}, // Draw with Scissors
+};
+
+static const std::unordered_map<std::string, uint32_t> outcomeScopeMapPart2 = {
+    {"A X", loss + scissorsValue}, // Lose against Rock
+    {"A Y", draw + rockValue},     // Draw against Rock
+    {"A Z", win + paperValue},     // Win against Rock
+    {"B X", loss + rockValue},     // Lose against Paper
+    {"B Y", draw + paperValue},    // Draw against Paper
+    {"B Z", win + scissorsValue},  // Win against Paper
+    {"C X", loss + paperValue},    // Lose against Scissors
+    {"C Y", draw + scissorsValue}, // Draw against Scissors
+    {"C Z", win + rockValue},      // Win against Scissors
+};
+
+void solve(const Lines &lines,
+           const std::unordered_map<std::string, uint32_t> &outcomeScopeMap) {
+  uint32_t total{0};
+  for (const auto &line : lines) {
+    if (line.empty()) {
+      continue;
+    }
+    const auto it = outcomeScopeMap.find(line);
+    if (it != outcomeScopeMap.end()) {
+      total += it->second;
+    } else {
+      std::cerr << "Unknown outcome: " << line << std::endl;
+    }
+  }
+  std::cout << total << std::endl;
+}
+
+int main(int /*argc*/, char **argv) {
+  Lines lines = read_lines(argv[1]);
+  solve(lines, outcomeScopeMapPart1);
+  solve(lines, outcomeScopeMapPart2);
+  return 0;
+}
