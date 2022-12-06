c698cb9c7f50d7d82d67a72f2789595d541b869b
Fibbo <1519902+fibbo@users.noreply.github.com>
2022-12-06 08:10:23 +0100

    Day 6

--- 06/day6.cpp
+++ 06/day6.cpp
@@ -1,13 +1,47 @@
 #include "aoc_lib.hpp"
+#include <algorithm>
+#include <array>
+#include <deque>
+#include <iostream>
+#include <unordered_set>
 
 using namespace pgl::aoc;
+
+uint32_t findSequenceStart(const std::string &sequence,
+                           uint32_t uniqueCharacters) {
+  size_t positionMarker{0};
+  std::vector<char> buffer;
+
+  for (; positionMarker < sequence.size(); ++positionMarker) {
+    buffer.push_back(sequence[positionMarker]);
+    if (buffer.size() < uniqueCharacters) {
+      continue;
+    }
+
+    auto checkForUnique = buffer;
+    std::sort(checkForUnique.begin(), checkForUnique.end());
+    auto last = std::unique(checkForUnique.begin(), checkForUnique.end());
+    checkForUnique.erase(last, checkForUnique.end());
+    if (checkForUnique.size() == uniqueCharacters) {
+      break;
+    }
+    buffer.erase(buffer.begin());
+  }
+  return ++positionMarker;
+}
 
 int main(int argc, char **argv) {
   Lines lines;
   if (argc == 2) {
     lines = read_lines(argv[1]);
   } else {
-    lines = read_lines("input.txt");
+    lines = read_lines("06/input_example.txt");
   }
+
+  for (const auto &line : lines) {
+    std::cout << findSequenceStart(line, 4) << std::endl;
+    std::cout << findSequenceStart(line, 14) << std::endl;
+  }
+
   return 0;
 }
