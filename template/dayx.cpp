f531945b3a68170943885fabea8e203518473d49
Fibbo <1519902+fibbo@users.noreply.github.com>
2022-12-07 22:22:54 +0100

    Prepare day 8, add template

---
+++ template/dayx.cpp
@@ -0,0 +1,26 @@
+#include "aoc_lib.hpp"
+
+#include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
+using namespace pgl::aoc;
+
+int main(int argc, char** argv) {
+  std::string fileName;
+  if (argc == 2) {
+    fileName = argv[1];
+  } else {
+    fileName = "x/input_example.txt";
+  }
+
+  [[maybe_unused]] bool runExample{false};
+  if (fileName.find("example") != std::string::npos) {
+    runExample = true;
+  }
+
+  const auto lines = read_lines(argv[1]);
+
+  return 0;
+}
