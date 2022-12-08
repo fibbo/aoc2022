82d70bbb814e357a35245616191ed7390e579465
Fibbo <1519902+fibbo@users.noreply.github.com>
2022-12-08 23:03:15 +0100

    Day 9

--- 09/day9.cpp
+++ 09/day9.cpp
@@ -12,7 +12,7 @@ int main(int argc, char** argv) {
   if (argc == 2) {
     fileName = argv[1];
   } else {
-    fileName = "x/input_example.txt";
+    fileName = "09/input_example.txt";
   }
 
   [[maybe_unused]] bool runExample{false};
